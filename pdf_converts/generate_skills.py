0e375f01bba03f21701a98f73cf4c1a8e8ecee7e
Manuel Carrero <mcomercio.web@gmail.com>
2020-02-28 02:24:57 -0400

    Adding progress getting skills after convert data from pdf

--- pdf_converts/generate_skills.py
+++ pdf_converts/generate_skills.py
@@ -1,10 +1,29 @@
 # -*- coding: utf-8 -*-
 import os
 from tika import parser
-from settings import *
+from configparser import RawConfigParser
+import json
+
+BASE_DIR = os.getcwd()
+config = RawConfigParser()
+config.read(BASE_DIR + '/settings.ini')
+
+PATH_FILE = config.get('pathFile', 'PATH_FILE')
+
+def get_skill(category_skills_array,categoryId):
+	data = []
+	for index,skill in enumerate(category_skills_array):
+		if index > 1:
+			data.append({
+				"isActive": True,
+				"isDeleted": False,
+				"name": skill,
+				"categoryId": categoryId
+			})
+	return data
 
 def main():
-	categories = {}
+	skills_mongo_collection = []
 	file = PATH_FILE
 
 	raw_data = parser.from_file(file)
@@ -16,40 +35,17 @@ def main():
 	# (previousyly done directly in the document)
 	cleaned_data = cleaned_data.split('*')
 
-	categories['Courier services'] = cleaned_data[1].split('#')
+	i=1
+	while(i !=18):
+		skills_mongo_collection.append(
+			get_skill(
+				cleaned_data[i].split('#'),
+				cleaned_data[i].split('#')[0])
+			)
+		i+=1
 
-	categories['Web Design'] = cleaned_data[2].split('#')
-
-	categories['Cleaning services'] = cleaned_data[3].split('#')
-
-	categories['Tutoring services'] = cleaned_data[4].split('#')
-
-	categories['Plumbing services'] = cleaned_data[5].split('#')
-
-	categories['Architects skills'] = cleaned_data[6].split('#')
-
-	categories['Care services'] = cleaned_data[7].split('#')
-
-	categories['Beauty&Health services'] = cleaned_data[8].split('#')
-
-	categories['Repair and construction'] = cleaned_data[9].split('#')
-
-	categories['Shipping'] = cleaned_data[10].split('#')
-
-	categories['Virtual assistant'] = cleaned_data[11].split('#')
-
-	categories['Computer services'] = cleaned_data[12].split('#')
-
-	categories['Photo and video services'] = cleaned_data[13].split('#')
-
-	categories['Events and promotions'] = cleaned_data[14].split('#')
-
-	categories['Installation and repair of machinery/ Repair of digital equipment'] = cleaned_data[15].split('#')
-
-	categories['Legal services'] = cleaned_data[16].split('#')
-
-	categories['Repair of vehicles'] = cleaned_data[17].split('#')
-
+	with open(BASE_DIR + "/skills_mongo_collection.json", "+w") as text_file:
+		text_file.write(json.dumps(skills_mongo_collection))
 
 if __name__ == '__main__':
     main()
