42abe140f838004939a19d5da7449f74fb18444c
ubuntu <manuel@anincubator.com>
2020-02-28 18:11:24 -0400

    Adding progress generating skills

--- pdf_converts/generate_skills.py
+++ pdf_converts/generate_skills.py
@@ -10,16 +10,54 @@ config.read(BASE_DIR + '/settings.ini')
 
 PATH_FILE = config.get('pathFile', 'PATH_FILE')
 
-def get_skill(category_skills_array,categoryId):
+def get_skill(category_skills_array,categoryId,category_index):
 	data = []
+	# Categories list
+	'''
+	"Courier services",
+	"Design",
+	"Cleaning services",
+	"Tutoring services",
+	"Plumbing services",
+	"Care",
+	"Beauty&Health services",
+	"Repair and construction",
+	"Shipping",
+	"Virtual assistant",
+	"Computer services",
+	"Photo and video services",
+	"Events and promotions",
+	"Installation and repair of machinery",
+	"Repair of digital equipment",
+	"Legal services",
+	"Repair of vehicles"
+	'''
+	categories_id = [
+		"5e000580c76a8e047da94770",
+		"5e000580c76a8e047da94771",
+		"5e000580c76a8e047da94772",
+		"5e000580c76a8e047da94773",
+		"5e000580c76a8e047da94774",
+		"5e000580c76a8e047da94775",
+		"5e000580c76a8e047da94776",
+		"5e000580c76a8e047da94777",
+		"5e000580c76a8e047da94778",
+		"5e000580c76a8e047da94779",
+		"5e000580c76a8e047da9477a",
+		"5e000580c76a8e047da9477b",
+		"5e000580c76a8e047da9477c",
+		"5e000580c76a8e047da9477d",
+		"5e000580c76a8e047da9477e",
+		"5e000580c76a8e047da9477e",
+		"5e000580c76a8e047da9477f",
+		"5e000580c76a8e047da94780"
+	]
+	# Structure template needed:
+	# {"_id":{"$oid":"<id>"},"isActive":true,"isDeleted":false,"name":"<name>","createAt":{"$date":{"$numberLong":"1577126355672"}},"updateAt":{"$date":{"$numberLong":"1577126355672"}},"__v":{"$numberInt":"0"}}
 	for index,skill in enumerate(category_skills_array):
-		if index > 1:
-			data.append({
-				"isActive": True,
-				"isDeleted": False,
-				"name": skill,
-				"categoryId": categoryId
-			})
+		if index > 0:
+			data.append('{"_id":{"$oid":' + categories_id[category_index] + '},"isActive": true,"isDeleted": false,"name":"' + skill + '","createAt":{"$date":{"$numberLong":"1577126355672"}},"updateAt":{"$date":{"$numberLong":"1577126355672"}},"__v":{"$numberInt":"0"}}')
+		#print(category_index,"--",index,"--",skill)
 	return data
 
 def main():
@@ -35,17 +73,20 @@ def main():
 	# (previousyly done directly in the document)
 	cleaned_data = cleaned_data.split('*')
 
-	i=1
-	while(i !=18):
+for i in range(1,18):
+	#print(i,cleaned_data[i].split('#'),cleaned_data[i].split('#')[0],"\n")
+	if i == 6:	# Architects skills
+		pass
+	else:
 		skills_mongo_collection.append(
 			get_skill(
 				cleaned_data[i].split('#'),
-				cleaned_data[i].split('#')[0])
+				cleaned_data[i].split('#')[0],
+				i)
 			)
-		i+=1
 
-	with open(BASE_DIR + "/skills_mongo_collection.json", "+w") as text_file:
-		text_file.write(json.dumps(skills_mongo_collection))
+with open(BASE_DIR + "/skills_mongo_collection.json", "+w") as text_file:
+	text_file.write(str(skills_mongo_collection))
 
 if __name__ == '__main__':
     main()
