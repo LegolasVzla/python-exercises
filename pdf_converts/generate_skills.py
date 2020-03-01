dd4c476ba926a5ba2dde074773f9749304bb09dc
Manuel Carrero <mcomercio.web@gmail.com>
2020-03-01 01:12:24 -0400

    Fixing bug getting categories from pdf file

--- pdf_converts/generate_skills.py
+++ pdf_converts/generate_skills.py
@@ -10,15 +10,16 @@ config.read(BASE_DIR + '/settings.ini')
 
 PATH_FILE = config.get('pathFile', 'PATH_FILE')
 
-def get_skill(category_skills_array,categoryId,category_index):
+def get_skill(skills_by_category_array,category_index):
 	data = []
-	# Categories list
+	# Categories list (18)
 	'''
 	"Courier services",
 	"Design",
 	"Cleaning services",
 	"Tutoring services",
 	"Plumbing services",
+	"Architects",
 	"Care",
 	"Beauty&Health services",
 	"Repair and construction",
@@ -38,6 +39,7 @@ def get_skill(category_skills_array,categoryId,category_index):
 		"5e000580c76a8e047da94772",
 		"5e000580c76a8e047da94773",
 		"5e000580c76a8e047da94774",
+		"5e000580c76a8e047da94781",
 		"5e000580c76a8e047da94775",
 		"5e000580c76a8e047da94776",
 		"5e000580c76a8e047da94777",
@@ -48,16 +50,27 @@ def get_skill(category_skills_array,categoryId,category_index):
 		"5e000580c76a8e047da9477c",
 		"5e000580c76a8e047da9477d",
 		"5e000580c76a8e047da9477e",
-		"5e000580c76a8e047da9477e",
 		"5e000580c76a8e047da9477f",
 		"5e000580c76a8e047da94780"
 	]
 	# Structure template needed:
 	# {"_id":{"$oid":"<id>"},"isActive":true,"isDeleted":false,"name":"<name>","createAt":{"$date":{"$numberLong":"1577126355672"}},"updateAt":{"$date":{"$numberLong":"1577126355672"}},"__v":{"$numberInt":"0"}}
-	for index,skill in enumerate(category_skills_array):
-		if index > 0:
-			data.append('{"_id":{"$oid":' + categories_id[category_index] + '},"isActive": true,"isDeleted": false,"name":"' + skill + '","createAt":{"$date":{"$numberLong":"1577126355672"}},"updateAt":{"$date":{"$numberLong":"1577126355672"}},"__v":{"$numberInt":"0"}}')
+
+	for index,skill in enumerate(skills_by_category_array):
 		#print(category_index,"--",index,"--",skill)
+
+		# index = 0 is the category Name
+		if index > 0 and category_index < 15:
+			data.append('{"categoryId":"' + categories_id[category_index] + '","isActive": true,"isDeleted": false,"name":"' + skill + '","createAt":{"$date":{"$numberLong":"1577126355672"}},"updateAt":{"$date":{"$numberLong":"1577126355672"}},"__v":{"$numberInt":"0"}}')
+
+			# Installation and repair of machinery/ Repair of digital equipment in pdf file
+			if category_index == 14:
+				data.append('{"categoryId":"' + categories_id[category_index+1] + '","isActive": true,"isDeleted": false,"name":"' + skill + '","createAt":{"$date":{"$numberLong":"1577126355672"}},"updateAt":{"$date":{"$numberLong":"1577126355672"}},"__v":{"$numberInt":"0"}}')
+
+		# Legal services and Repair of vehicles in pdf file
+		if index > 0 and category_index >= 15:
+			data.append('{"categoryId":"' + categories_id[category_index+1] + '","isActive": true,"isDeleted": false,"name":"' + skill + '","createAt":{"$date":{"$numberLong":"1577126355672"}},"updateAt":{"$date":{"$numberLong":"1577126355672"}},"__v":{"$numberInt":"0"}}')
+
 	return data
 
 def main():
@@ -73,20 +86,19 @@ def main():
 	# (previousyly done directly in the document)
 	cleaned_data = cleaned_data.split('*')
 
-for i in range(1,18):
-	#print(i,cleaned_data[i].split('#'),cleaned_data[i].split('#')[0],"\n")
-	if i == 6:	# Architects skills
-		pass
-	else:
+	for i in range(1,18):
+		#print(i,cleaned_data[i].split('#'),cleaned_data[i].split('#')[0],"\n")
 		skills_mongo_collection.append(
 			get_skill(
 				cleaned_data[i].split('#'),
-				cleaned_data[i].split('#')[0],
-				i)
+				i-1)
 			)
 
-with open(BASE_DIR + "/skills_mongo_collection.json", "+w") as text_file:
-	text_file.write(str(skills_mongo_collection))
+	# Remove ''
+	cleaned_data = str(skills_mongo_collection).replace("'",'')
+
+	with open(BASE_DIR + "/skills_mongo_collection.json", "+w") as text_file:
+		text_file.write(cleaned_data)
 
 if __name__ == '__main__':
     main()
